0565410173500b3300365bd7521dd25fdea49ad0
Dawid Trojanowski <exea.centrum@gmail.com>
2024-02-15 19:42:25 +0100

    add commit mikrotik comarch sql6

--- Comarch-cdnXL-sql/dbo.prd.sql
+++ Comarch-cdnXL-sql/dbo.prd.sql
@@ -1,8 +1,8 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT     CDN.ZlcNag.ZcN_GIDNumer, CDN.ZlcNag.ZcN_ZlcSeria, CDN.ZlcNag.ZcN_ZlcNumer, CDN.ZlcNag.ZcN_DataRealizacji, CDN.ZlcElem.ZcE_TwrNazwa, CDN.ZlcElem.ZcE_KosztEwidencyjny, 
                       CDN.KntKarty.Knt_Akronim, 
-                      CASE CDN.KntKarty.Knt_Akronim WHEN 'OLKIEWICZ JADWIGA' THEN 'JO' WHEN 'CURY£O RADOS£AW' THEN 'RC' WHEN 'KRASUCKI £UKASZ' THEN 'LK' WHEN 'ŒWITALSKI MAREK' THEN 'MS' WHEN
-                       '¯URAWSKI KRZYSZTOF' THEN 'KZ' ELSE 'PRD' END AS Expr1
+                      CASE CDN.KntKarty.Knt_Akronim WHEN 'JADWIGA' THEN 'JO' WHEN 'RADOSï¿½AW' THEN 'RC' WHEN 'ï¿½UKASZ' THEN 'LK' WHEN 'MAREK' THEN 'MS' WHEN
+                       'KRZYSZTOF' THEN 'KZ' ELSE 'PRD' END AS Expr1
 FROM         CDN.ZlcNag INNER JOIN
                       CDN.ZlcElem ON CDN.ZlcNag.ZcN_GIDNumer = CDN.ZlcElem.ZcE_GIDNumer INNER JOIN
                       CDN.KntKarty ON CDN.ZlcNag.ZcN_KntNumer = CDN.KntKarty.Knt_GIDNumer
