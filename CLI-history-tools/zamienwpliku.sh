d35d09d056d75a864203efc6ee9b4e0537512b14
Dawid Trojanowski <exea.centrum@gmail.com>
2024-02-15 11:22:12 +0100

    add commit cli-tools

---
+++ CLI-history-tools/zamienwpliku.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+sed -e 's|Mam|one|' -e 's|brak|two|' -e 's|3|three|' pytanie_bashmainternet2
+
+# sed -e '
+#  
+# > s/Brak/Brak2222/
+#  
+# > s/Ma/Ma2222/' pytanie_bashmainternet2
+
+
+##############################
+# sed '
+# s/a/A/g 
+# s/e/E/g 
+# s/i/I/g 
+# s/o/O/g 
+# s/u/U/g'  pytanie_bashmainternet
